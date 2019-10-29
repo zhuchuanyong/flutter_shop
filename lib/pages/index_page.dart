fbb2269405615fbb1e994108a0110e723cc0ffbf
zhuchuanyong <zhuchuanyong@vtstar.net>
2019-10-29 23:28:08 +0800

    fea:底部bar

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
-class IndexPage extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'cart_page.dart';
+import 'category_page.dart';
+import 'home_page.dart';
+import 'member_page.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  // 定义底部 bar信息
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text('首页')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search), title: Text('分类')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart), title: Text('购物车')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.profile_circled), title: Text('会员中心'))
+  ];
+  // 定义底部页面
+  final List<Widget> tabBodies = [HomePage(), CarPage(), MemberPage(), CategoryPage()];
+  int currentIndex = 0; // 当前页面索引
+  var currentPage; // 当前页面
+  @override
+  void initState() {
+    // TODO: implement initState
+    // 设置初始值 初始页面
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百姓生活+'),
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0), // 背景颜色
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // bar类型
+        currentIndex: currentIndex, // 当前索引
+        items: bottomTabs, // 底部bar信息
+        onTap: (index) { // 点击事件
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
       ),
-      body: Center(
-        child: Text('函数式'),
-      ),
+      body: currentPage,
     );
   }
 }
