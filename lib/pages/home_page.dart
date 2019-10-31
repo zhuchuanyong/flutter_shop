f88653e47bbef8af2d62ce942a4e2f00f7a547e0
zhuchuanyong <zhuchuanyong@vtstar.net>
2019-10-31 23:23:12 +0800

    fea:首页轮播

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -26,8 +29,43 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('百姓生活'),
       ),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = jsonDecode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDateList:swiper)
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+  SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network('${swiperDateList[index]['image']}',fit: BoxFit.fill,);
+        },
+        itemCount: swiperDateList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
