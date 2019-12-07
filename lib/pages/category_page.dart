7ca99674291b32cb4d1a7f587f9f5af7ee6a12e5
zhuchuanyong <zhuchuanyong@vtstar.net>
2019-12-07 16:01:08 +0800

    '23课分类页左侧类别导航'

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/service/service_method.dart';
 import '../model/category.dart';
 
@@ -12,18 +13,71 @@ class CategoryPage extends StatefulWidget {
 
 class _CategoryPageState extends State<CategoryPage> {
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[LeftCatgegoryNav()],
+        ),
+      ),
+    );
+  }
+}
+
+//左侧类别导航
+class LeftCatgegoryNav extends StatefulWidget {
+  @override
+  _LeftCatgegoryNavState createState() => _LeftCatgegoryNavState();
+}
+
+class _LeftCatgegoryNavState extends State<LeftCatgegoryNav> {
+  List list = [];
+
+  @override
   void initState() {
-    // TODO: implement initState
-    super.initState();
     _getCategory();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    _getCategory();
     return Container(
-      child: Center(
-        child: Text('类别'),
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(color: Colors.black12, width: 1),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return _leftInlWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInlWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12),
+          ),
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(
+            fontSize: ScreenUtil().setSp(28),
+          ),
+        ),
       ),
     );
   }
@@ -33,8 +87,11 @@ class _CategoryPageState extends State<CategoryPage> {
       // print(val);
       var data = json.decode(val.toString());
       // print(data);
-      CategoryModel list = CategoryModel.fromJson(data);
-      list.data.forEach((item) => print(item.mallCategoryName));
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+        list = category.data;
+      });
+      // list.data.forEach((item) => print(item.mallCategoryName));
     });
   }
 }
